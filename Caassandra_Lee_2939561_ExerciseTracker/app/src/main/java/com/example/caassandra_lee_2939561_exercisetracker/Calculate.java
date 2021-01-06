603e06a362c1a9810eeccafd375bfc40f72d8106
Cass <casslee134@yahoo.com>
2021-01-06 22:08:21 +0000

    Milestone 1

---
+++ Caassandra_Lee_2939561_ExerciseTracker/app/src/main/java/com/example/caassandra_lee_2939561_exercisetracker/Calculate.java
@@ -0,0 +1,52 @@
+package com.example.caassandra_lee_2939561_exercisetracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class Calculate extends AppCompatActivity {
+
+    Button stop;
+    TextView text1;
+    TextView text2;
+    TextView text3;
+    TextView text4;
+    TextView text5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+    public void calculateDistance(){
+
+
+    }
+
+    public void calculateSpeed(){
+
+    }
+
+    public void calculateTime(){
+
+    }
+
+    public void calculateAltitude(){
+
+    }
+
+    public void Map(){
+
+
+    }
+
+}
---
+++ Caassandra_Lee_2939561_ExerciseTracker/app/src/main/java/com/example/caassandra_lee_2939561_exercisetracker/Track.java
@@ -0,0 +1,38 @@
+package com.example.caassandra_lee_2939561_exercisetracker;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class Track extends AppCompatActivity {
+
+    Button start;
+    Button stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+   public void startTracker(){
+
+
+   }
+
+   public void stopTracker(){
+
+
+   }
+}
